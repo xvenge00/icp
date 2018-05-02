f12d2d47d47963491db843de48af1b1b1212a379
Matej Kastak <matej.kastak@gmail.com>
2018-05-03 00:27:50 +0200

    ConnectionGraphicsObject trieda
    kostra triedy reprezentujuca objekt spojenia pre vykreslenie

---
+++ src/connection_graphics_object.cpp
@@ -0,0 +1,18 @@
+#include "connection_graphics_object.h"
+#include "debug.h"
+
+ConnectionGraphicsObject::ConnectionGraphicsObject(Connection *c) {
+    this->_connection = c;
+}
+
+void ConnectionGraphicsObject::paint(QPainter *painter,
+                                const QStyleOptionGraphicsItem *option,
+                                QWidget *widget) {
+    painter->drawText(100, 100, 100, 100, Qt::AlignCenter, "This is connection test");
+}
+
+QRectF ConnectionGraphicsObject::boundingRect() const {
+    // LOGE("NOT IMPLEMENTED!");
+    return {};
+}
+
---
+++ src/connection_graphics_object.h
@@ -0,0 +1,28 @@
+#ifndef ICP_CONNECTION_GRAPHICS_OBJECT_H
+#define ICP_CONNECTION_GRAPHICS_OBJECT_H
+
+#include <QGraphicsObject>
+#include <QPainter>
+#include <QPoint>
+
+#include "connection.h"
+
+class ConnectionGraphicsObject : public QGraphicsObject {
+  public:
+    ConnectionGraphicsObject(Connection *c);
+
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
+               QWidget *widget);
+
+    QRectF boundingRect() const Q_DECL_OVERRIDE;
+
+    Connection *getConnection() { return this->_connection; };
+
+    //friend std::ostream &operator<<(std::ostream &s, const BlockGraphicsObject &b); 
+
+  private:
+    Connection *_connection;
+
+};
+
+#endif // ICP_CONNECTION_GRAPHICS_OBJECT_H
